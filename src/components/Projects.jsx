0160624cb096649f2132d4373aab13772c1ef930
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-12-12 17:31:03 +0600

    added vercel json folder

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
         <div className="overflow-x-hidden">
             <Navbar></Navbar>
             <h2 className="text-5xl text-center font-bold mt-8">Projects</h2>
-            <p className="text-lg text-center my-4"> Projects I am in as a developer</p>
+            <p className="text-lg text-center my-4"> Projects I build as a developer</p>
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
            <div className="card w-96 glass">
                 <figure><img className="w-96" src="https://i.ibb.co/rGgxbZK/screencapture-lush-beauty-client-web-app-2023-12-12-12-15-18.png" alt="car!" /></figure>
@@ -36,7 +36,7 @@ Deliver a visually captivating user interface for hotel room bookings,enhancing 
             <div className="card w-96 glass">
                 <figure><img className="w-96" src="https://i.ibb.co/JBnkY0v/screencapture-byte-blitz-client-web-app-2023-12-12-12-21-22.png" alt="car!" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Lush Beauty</h2>
+                    <h2 className="card-title">Byte blitz</h2>
 
                     <p>Discover and engage with the latest tech products on our platform, featuring an interactive system for users to upvote, submit, and explore innovations.</p>
                     <div className="card-actions justify-end">
