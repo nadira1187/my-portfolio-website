f3ee0136ec1470f5b35948cbc4a29bbfe9b49d7f
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-12-12 15:05:19 +0600

    fixing issue in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,19 +38,12 @@ const Navbar = () => {
     <div>
       <div className="navbar  max-w-screen-xl bg-base-100  ">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-4">
-              {links}
-            </ul>
-          </div>
+          
 
-          <a className="btn btn-ghost normal-case invisible  md:visible text-red-900  text-4xl font-bold">
-            <img className='w-12' src="https://i.ibb.co/5nPH0Qm/letter-j-9871830.png" alt="icon1" border="0" />JK | Nadira</a>
+          <a className="btn btn-ghost normal-case  text-red-900  text-4xl font-bold">
+            <img className='w-12' src="https://i.ibb.co/5nPH0Qm/letter-j-9871830.png" alt="icon1" border="0" /><span className='invisible  md:visible'>JK | Nadira</span></a>
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-center  lg:flex">
           <ul className="menu menu-horizontal gap-4 px-1">
             {links}
           </ul>
