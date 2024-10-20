97aeb3ec1ac8ec42e271798ce426d92b9706e91d
nadira1187 <nadira35-1187@s.diu.edu.bd>
2024-10-20 23:04:56 +0600

    added more responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
+    <div className='max-w-screen-xl mx-auto overflow-hidden'>
     <RouterProvider router={router} />
     </div>
    
